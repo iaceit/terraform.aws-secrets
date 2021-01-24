129fa71eeebf3fb2448f98a81a15c36a62bf7e8f
Haoming Yin <haomingyin@users.noreply.github.com>
2021-01-24 20:12:06 +1300

    Moved to 2021 AWS account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 0.12"
 
   backend "s3" {
-    region = "us-east-1"
-    bucket = "iaceit.com"
+    region = "ap-southeast-2"
+    bucket = "987882637451"
     key    = "iaceit.com/terraform/aws-secrets/ap-southeast-2/terraform.tfstate"
   }
 }
